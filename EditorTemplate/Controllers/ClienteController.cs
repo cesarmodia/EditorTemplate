713c8457b9fa43a58988fa7a5b73213a6f4fe7dc
Cesar Morales <cmorales@thedataage.com>
2017-06-19 11:09:43 -0600

    Realización de una plantilla que ubica los valores de una propiedad de tipo ENUM de un modelo en una vista utilizando el helper HTML.EDITOR

---
+++ EditorTemplate/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EditorTemplate.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Edit()
+        {
+            return View(new Models.Cliente());
+        }
+    }
+}
---
+++ EditorTemplate/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EditorTemplate.Models
+{
+    public enum TipoCliente
+    {
+        General,
+        Vip
+    }
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        [UIHint("Enum")]
+        [DisplayName("Tipo de cliente")]
+        public TipoCliente tipoCliente { get; set; }
+    }
+}
