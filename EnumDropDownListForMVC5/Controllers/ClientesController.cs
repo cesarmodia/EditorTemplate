3be91e6c00fa236063b64b383feb0f68f76dac0f
Cesar Morales <cmorales@thedataage.com>
2017-06-19 17:15:06 -0600

    Creación de Template para que el enum se presente como un grupo de selección de radio button

---
+++ EnumDropDownListForMVC5/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnumDropDownListForMVC5.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View(new Models.ViewModelCliente());
+        }
+    }
+}
---
+++ EnumDropDownListForMVC5/Models/ViewModelCliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EnumDropDownListForMVC5.Models
+{
+    public enum TipoCliente
+    {
+        General = 0,
+        Asociado = 1,
+        Vip = 2,
+        Propietario = 3
+    }
+    public class ViewModelCliente
+    {
+        [Required()]
+        [Display(Name = "Id. del Cliente")]
+        public int idCliente { get; set; }
+        [StringLength(50)]
+        public string Nombre { get; set; }
+        public Nullable<TipoCliente> Tipo { get; set; }
+    }
+}
